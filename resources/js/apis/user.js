32555582ff1545a94e95f63f836d826b81edf65a
christian <christian.igay@gmail.com>
2022-09-23 15:48:31 +0800

    add apiDetails

--- resources/js/apis/user.js
+++ resources/js/apis/user.js
@@ -2,4 +2,4 @@ import { resource_admin_api, Axios } from "./adminApi"
 const resource = 'user'
 
 export const apiRegister = (payload) => Axios.post(`${resource_admin_api}/${resource}/insert`, payload)
-export const apiDetails = (payload) => Axios.get(`${resource_admin_api}/${resource}/details`)
+export const apiDetails = () => Axios.get(`${resource_admin_api}/${resource}/details`)
