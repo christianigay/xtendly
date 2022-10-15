a873d93ec655e0a7e00e79a16c0c4c7a399f8ccb
christian igay <christian.igay@gmail.com>
2022-10-15 22:14:37 +0800

    fix home responsive

--- resources/js/pages/home/index.vue
+++ resources/js/pages/home/index.vue
@@ -1,62 +1,74 @@
 <template>
     <div class="q-mx-lg home-wrapper">
 		<!-- <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" /> -->
-		<div class="box-wrapper"></div>
-			<div class="column items-center">
-				<div class="col">
-					<div class="search-wrapper-desc">
-						<div style="display: flex">
-							<div class="logo-hash">#</div>
-							<div class="logo-f">F</div>
-							<div class="logo-first">Flo</div>
-							<div class="logo-qast">Qast</div>
-							<div class="logo-the">The</div>
+				<div class="box-wrapper search-desktop"></div>
+				<div class="box-wrapper-mobile"></div>
+				<div class="column items-center search-desktop">
+					<div class="col">
+						<div class="search-wrapper-desc">
+							<div style="display: flex">
+								<div class="logo-hash">#</div>
+								<div class="logo-f">F</div>
+								<div class="logo-first">Flo</div>
+								<div class="logo-qast">Qast</div>
+								<div class="logo-the">The</div>
+							</div>
+							<div class="fastest-text">Fastest, Most Accurate</div>
+							<div class="way-text">Way to Close Your Books!</div>
 						</div>
-						<div class="fastest-text">Fastest, Most Accurate</div>
-						<div class="way-text">Way to Close Your Books!</div>
 					</div>
-				</div>
-
-				<div class="col search">
-					<div class="search-wrapper">
-						<div id="pointer">
-							<div class="wrapper">
-									<div class="header">SCHEDULE A DEMO</div>
-									<div class="subheader">Learn More About FloQast</div>
+					<div class="col search">
+						<div class="search-wrapper">
+							<div id="pointer">
+								<div class="wrapper">
+										<div class="header">SCHEDULE A DEMO</div>
+										<div class="subheader">Learn More About FloQast</div>
+								</div>
+							</div>
+							<div class="search-form">
+								<div class="description"><span class="desc1">Learn How FloQast Can </span><span class="desc2"> Your Month-End</span></div>
+								<div class="form-input" style="display: flex;">
+									<input v-model="form.firstname" placeholder="First Name" class="q-mx-sm"/>
+									<input v-model="form.email" placeholder="Email"/>
+									<button class="q-ml-sm">SCHEDULE NOW</button>
+								</div>
 							</div>
 						</div>
-						<div class="search-form">
-							<div class="description"><span class="desc1">Learn How FloQast Can </span><span class="desc2"> Your Month-End</span></div>
-							<div class="form-input" style="display: flex;">
-								<input v-model="form.firstname" placeholder="First Name" class="q-mx-sm"/>
-								<input v-model="form.email" placeholder="Email"/>
-								<button class="q-ml-sm">SCHEDULE NOW</button>
+					</div>
+				</div>
+
+				<div class="column items-center search-mobile-section">
+					<div class="col">
+						<div class="search-mobile-wrapper-desc">
+							<div style="display: flex">
+								<div class="logo-hash">#</div>
+								<div class="logo-f">F</div>
+								<div class="logo-first">Flo</div>
+								<div class="logo-qast">Qast</div>
+								<div class="logo-the">The</div>
+							</div>
+						</div>
+					</div>
+
+					<div class="col" style="margin-top: 22px;">
+						<div class="search-mobile search-mobile-wrapper">
+							<div class="mobile-header">SCHEDULE A DEMO</div>
+							<div class="mobile-subheader">Learn More About FloQast</div>
+						</div>
+						<div class="search-mobile-body">
+							<div class="description-mobile" style="text-align: center;">
+								<div class="desc1">Learn How FloQast Can </div>
+								<div class="desc2"> Your Month-End</div>
+							</div>
+							<div class="mobile-form-input">
+								<input v-model="form.firstname" placeholder="First Name" class="mob-form"/>
+								<input v-model="form.email" placeholder="Email" class="mob-form"/>
+								<button class="mobile-button" type="submit">SCHEDULE NOW</button>
 							</div>
 						</div>
 					</div>
 				</div>
 
-			</div>
-
-			<div class="column items-center search-mobile-section">
-				<div class="search-mobile">
-					<div class="search-mobile-wrapper">
-						<div>SCHEDULE A DEMO</div>
-						<div>Learn More About FloQast</div>
-					</div>
-					<div class="search-mobile-body">
-						<div class="description-mobile" style="text-align: center;">
-							<div class="desc1">Learn How FloQast Can </div>
-							<div class="desc2"> Your Month-End</div>
-						</div>
-						<div class="mobile-form-input">
-							<input v-model="form.firstname" placeholder="First Name"/>
-							<input v-model="form.email" placeholder="Email"/>
-						</div>
-						<button class="mobile-button" type="submit">SCHEDULE NOW</button>
-					</div>
-				</div>
-			</div>
     </div>
 </template>
 <script>
@@ -96,26 +108,37 @@ export default {
 
 .box-wrapper {
 	background: #fff;
-    width: 50%;
-    height: 130px;
-    top: 45px;
-    position: absolute;
+	width: 50%;
+	height: 130px;
+	top: 45px;
+	position: absolute;
 	border-top-right-radius: 5px;
-    border-bottom-right-radius: 5px;
+	border-bottom-right-radius: 5px;
+	opacity: 40%;
+}
+
+.box-wrapper-mobile {
+	background: #fff;
+	width: 64%;
+	height: 82px;
+	top: 44px;
+	position: absolute;
+	border-top-right-radius: 5px;
+	border-bottom-right-radius: 5px;
 	opacity: 40%;
 }
 
 .fastest-text {
 	color: #9cc448;
-    font-size: 26px;
-    position: absolute;
-    top: 104px;
+	font-size: 26px;
+	position: absolute;
+	top: 104px;
 }
 
 .way-text {
 	font-size: 16px;
-    position: absolute;
-    top: 136px;
+	position: absolute;
+	top: 136px;
 }
 
 .logo-first {
@@ -262,7 +285,7 @@ export default {
 
 @media only screen and (min-width: 831px) {
   
-  .search-mobile {
+  .search-mobile-section, .box-wrapper-mobile {
 		display: none;
   }
 }
@@ -273,56 +296,71 @@ export default {
     width: 100%;
   }
 
-  .search {
+	.search-mobile-wrapper-desc {
+		text-align: left !important;
+		font-style: italic;
+		width: 332px;
+	}
+	
+  .search, .search-desktop {
 		display: none;
   }
 
-  .search-mobile-section {
-		margin-top: 46px;
-  }
-
-  .search-mobile-wrapper {
+	.search-mobile-wrapper {
 		text-align: center;
     background: #8bc540;
     color: #fff;
-    padding: 6px;
-    border-top-left-radius: 5px;
-    border-top-right-radius: 5px;
+    padding: 8px 0px;
+    border-top-left-radius: 6px;
+    border-top-right-radius: 6px;
   }
 
-  .search-mobile-body {
+	.search-mobile-body {
 		background: #fff;
-		padding: 12px 16px;
-		border-bottom-left-radius: 5px;
-    border-bottom-right-radius: 5px;
+		padding: 6px 0px;
+		border-bottom-left-radius: 6px;
+    border-bottom-right-radius: 6px;
   }
 
-	.mobile-form-input input {
+	.mobile-header {
+		font-weight: 700;
+	}
+
+	.mobile-subheader {
+		font-size: 13px;
+	}
+
+	.description-mobile .desc1 {
+		color: #77787a;
+		font-weight: 700;;
+	}
+
+	.description-mobile .desc2 {
+		color: #8bc540;
+		font-weight: 700;;
+	} 
+
+	.mobile-form-input {
+		width: 326px;
+		padding: 8px 14px
+	}
+
+	.mobile-form-input input, button {
 		width: 100%;
-		margin: 4px 0px;
 		border: 1px solid #d9d5d5;
 		border-radius: 5px;
 		padding: 3px 6px;
 	}
 
+	.mob-form {
+		margin: 5px 0px;
+	}
+
 	.mobile-button {
-		width: 100%;
-		margin-top: 8px;
 		color: #fff;
 		background: #9cc448;
+		margin-top: 4px;
 	}
 
-	.description-mobile {
-    font-weight: 700;
-    font-size: 16px;
-	}
-
-	.description-mobile .desc1 {
-		color: #77787a;
-	}
-
-	.description-mobile .desc2 {
-		color: #8bc540;
-	}
 }
 </style>
