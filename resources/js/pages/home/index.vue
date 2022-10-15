3f585980f7165e74282e2ed8e3dcc02a2fadcad0
christian igay <christian.igay@gmail.com>
2022-10-15 22:41:00 +0800

    git push opacity on mobile

--- resources/js/pages/home/index.vue
+++ resources/js/pages/home/index.vue
@@ -1,6 +1,5 @@
 <template>
     <div class="q-mx-lg home-wrapper">
-		<!-- <img src="https://images.unsplash.com/photo-1499750310107-5fef28a66643?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80" /> -->
 				<div class="box-wrapper search-desktop"></div>
 				<div class="box-wrapper-mobile"></div>
 				<div class="column items-center search-desktop">
